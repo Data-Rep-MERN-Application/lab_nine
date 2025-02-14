2e4ed88eb9c46124228ca94c9673042b9f1d196f
mkenirons <martin.kenirons@gmit.ie>
2025-02-14 10:23:21 +0000

    Closes #1

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,7 +4,7 @@ const port = 4000;
 
 const cors = require('cors');
 app.use(cors());
-
+//hello
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
